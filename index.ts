a3f1de25fa811fcd62c218f0d2f473524bd1839b
Dave Pringle <3244206+UncleDave@users.noreply.github.com>
2023-03-03 13:14:45 +0100

    add another bucket

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ const suffix = 'dkdavpri';
 
 // Create an AWS resource (S3 Bucket)
 const bucket = new aws.s3.Bucket(`best-bucket-ever-${ suffix }`);
+const secondBucket = new aws.s3.Bucket(`even-better-bucket-${ suffix }`);
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
